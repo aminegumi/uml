f614546d7dfeeccffd4e2887b676ab4c42f67a3d
aminegumi <mohammedamineagoumi@gmail.com>
2024-11-03 11:24:35 +0100

    Postgre configuration

--- uml-backend/api/models.py
+++ uml-backend/api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class UMLDiagram(models.Model):
+    name = models.CharField(max_length=255)
+    diagram = models.JSONField()  # This field will store the UML diagram in JSON format.
+
+    def __str__(self):
+        return self.name
